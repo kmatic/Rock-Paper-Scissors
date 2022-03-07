393ffacc6ecb3cb9fbc1e4f7cf3a7d0413f74656
Kristopher <kristopher_matic@hotmail.com>
2022-03-06 17:58:14 -0800

    Create playRound function

--- index.js
+++ index.js
@@ -10,5 +10,35 @@ function computerPlay() {
     }
 }
 
-const computerSelect = computerPlay();
-console.log(computerSelect);
+function playRound(playerSelection, computerSelection) {
+    let playerMove = playerSelection.toLowerCase();
+    if (playerMove === "rock") {
+        if (computerSelection === "Paper") {
+            return "You lose! Paper beats Rock";
+        } else if (computerSelection === "Scissors") {
+            return "You win! Rock beats Scissors"; 
+        } else {
+            return "Tie!";
+        }
+    } else if (playerMove === "paper") {
+        if (computerSelection === "Rock") {
+            return "You win! Paper beats Rock";
+        } else if (computerSelection === "Scissors") {
+            return "You lose! Scissors beats Paper";
+        } else {
+            return "Tie!";
+        }
+    } else {
+        if (computerSelection === "Rock") {
+            return "You lose! Rock beats Scissors";
+        } else if (computerSelection === "Paper") {
+            return "You win! Scissors beats Paper";
+        } else {
+            return "Tie!";
+        }
+    }
+}
+
+const playerSelection = "Rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
